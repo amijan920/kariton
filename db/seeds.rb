f929cb6d1abe3dba85c3eec0da7a0bd5fd9ac89a
Amiel Reveche <amielreveche@gmail.com>
2015-01-28 00:09:49 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create(name: "Bamboo Money Box", description: "A money box made exquisitely of bamboo perfect for saving money.", price:300.00, stock:54, image:"items/bamboo-money-box.png")
+
+Item.create(name: "Abaca Slippers", description: "Slippers made of pure Abaca. Designed by our world famous designer, Juliana Santada.", price:498.00, stock:102, image:"items/abaca.jpg")
+
+Item.create(name: "Narra Table", description: "A designer table made from one of the strongest wood in the Philippines, the Narra. Burnished and topped with tampered glass.", price:23000, stock:13, image:"items/narra-table.jpg")
+
+Item.create(name: "Bamboo Chair", description: "A chair made of bamboo. It's just the right item to make any house look native.", price:2499, stock:43, image:"items/bamboo-chair.png")
